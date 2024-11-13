f94261d72a98ac5305a2a6556e8fb5b6bda03eff
Dhimylee Almeida <dhimylee.almeida@gmail.com>
2024-11-12 22:14:46 -0300

    Feat: implementing css in functions

---
+++ wp-content/themes/investment/functions.php
@@ -0,0 +1,7 @@
+<?php
+//Carregamento de scripts e estilos
+function investment_scripts() {
+    wp_enqueue_style( 'investment-styles', get_template_directory_uri() . '/assets/css/style.css', array(), '1.0', 'all' );
+}
+add_action( 'wp_enqueue_scripts', 'investment_scripts' );
+?>
