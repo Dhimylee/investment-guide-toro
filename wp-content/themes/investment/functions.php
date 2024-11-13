fe7f41863c87b5c5fa733eba516936f1d4e64cc0
Dhimylee Almeida <dhimylee.almeida@gmail.com>
2024-11-13 14:09:13 -0300

    Feat: Add .htaccess for permalink structure and enable post thumbnails support

--- wp-content/themes/investment/functions.php
+++ wp-content/themes/investment/functions.php
@@ -4,4 +4,6 @@ function investment_scripts() {
     wp_enqueue_style( 'investment-styles', get_template_directory_uri() . '/assets/css/style.css', array(), '1.0', 'all' );
 }
 add_action( 'wp_enqueue_scripts', 'investment_scripts' );
+
+add_theme_support( 'post-thumbnails' );
 ?>
