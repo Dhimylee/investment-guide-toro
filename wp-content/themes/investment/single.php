00152499eb89e409e490378159590c33e04187be
Dhimylee Almeida <dhimylee.almeida@gmail.com>
2024-11-15 17:51:38 -0300

    feat: Adding single post page styles

--- wp-content/themes/investment/single.php
+++ wp-content/themes/investment/single.php
@@ -19,10 +19,10 @@
                                 </strong>
                                 <h1 class="single-post__title"><?php the_title(); ?></h1>
                                 <div class="single-post__post-data">
-                                    <span class="single-post__author"><img width="15px" src="<?php echo get_template_directory_uri(); ?>/assets/img/ico/user.svg" alt=""><?php echo get_the_author(); ?></span>
-                                    <time class="single-post__time"><img width="15px" src="<?php echo get_template_directory_uri(); ?>/assets/img/ico/calendar.svg" alt="">Atualizado: <?php echo get_the_modified_date('d/m/Y'); ?> às <?php echo get_the_modified_time('H:i'); ?></time>
+                                    <span class="single-post__author single-post--flex"><img width="20px" src="<?php echo get_template_directory_uri(); ?>/assets/img/ico/user.svg" alt=""><?php echo get_the_author(); ?></span>
+                                    <time class="single-post__time single-post--flex"><img width="20px" src="<?php echo get_template_directory_uri(); ?>/assets/img/ico/calendar.svg" alt="">Atualizado: <?php echo get_the_modified_date('d/m/Y'); ?> às <?php echo get_the_modified_time('H:i'); ?></time>
                                 </div>
-                                <div class="post-content">
+                                <div class="single-post__content">
                                     <?php the_content(); ?>
                                 </div>
                             </article>
@@ -31,7 +31,7 @@
                 </div>
 
                 <div class="single-post__column-2">
-                    <aside class="read-also">
+                    <aside class="single-post__aside">
                         <h3>Leia também</h3>
                         <?php
                         $categories = wp_get_post_categories(get_the_ID());
@@ -43,7 +43,10 @@
 
                         if ($related_posts->have_posts()) :
                             while ($related_posts->have_posts()) : $related_posts->the_post(); ?>
-                                <a href="<?php the_permalink(); ?>"><?php the_title(); ?></a>
+                                <a href="<?php the_permalink(); ?>">
+                                    <span class="link-content"><?php the_title(); ?></span>
+                                    <img class="arrow-icon" width="35px" src="<?php echo get_template_directory_uri(); ?>/assets/img/ico/arrow.svg" alt="">
+                                </a>
                             <?php endwhile;
                         endif;
                         ?>
